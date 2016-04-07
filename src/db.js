a46a1910a8cfa366c9d790d714c779505c4876a6
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2016-04-07 23:05:31 +0100

    close connection properly

--- src/db.js
+++ src/db.js
@@ -1,28 +1,11 @@
 "use strict";
-//
-// import mongoose from "mongoose";
-// import Promise from "bluebird";
-// import Person from "./schema/person";
 
 var Promise = require("bluebird");
-
-
 var mongoose = require("mongoose");
-
-Promise.promisifyAll(mongoose);
-
 
 var FullContactProfile;
 
 var MONGO_URL = "mongodb://localhost:27017/es6"
-
-// var _setupModel = ()=> {
-//     return mongoose.model("Person", {
-//         name  : String,
-//         title : String,
-//     }
-// };
-// mongoose.set('debug', true);
 
 var _connect = function() {
     var fullContactSchema = mongoose.Schema({
@@ -32,7 +15,6 @@ var _connect = function() {
 
   FullContactProfile = mongoose.model('FullContactProfile', fullContactSchema);
 
-
     return new Promise(function(resolve, reject) {
         mongoose.connect(MONGO_URL);
         mongoose.connection.once("open", resolve);
@@ -40,25 +22,10 @@ var _connect = function() {
     });
 };
 
-var insertPerson = function() {
-    var arvind = new FullContactProfile({
-        "email" : 'abdu@a.com',
-        "lastImported" : (new Date()).toISOString()
-    });
-
-    arvind.save(function (err, data) {
-        if (err) console.log(err);
-        else console.log('Saved : ', data );
-    });
-};
-
 var getPersons = function() {
     return new Promise(function(resolve, reject) {
-        console.log("exec");
-        FullContactProfile.findAsync(function (err, profiles) {
+        FullContactProfile.find(function (err, profiles) {
                 if(err) {
-                    console.log("err");
-
                     reject(err);
                 } else {
                     resolve(profiles);
@@ -69,21 +36,11 @@ var getPersons = function() {
 };
 
 _connect()
-    .then(function() {
-      console.log("connected");
-    })
-    // .then(insertPerson)
     .then(getPersons)
-    .then(function(profile) {
-        console.log(JSON.stringify(profile));
+    .then(function(profiles) {
+      console.log(">> " + profiles)
     })
-
-    // .then(mongoose.connection.closeAsync());
-
-    // If the Node process ends, close the Mongoose connection
-    process.on('SIGINT', function() {
-      mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-      });
+    .catch(function(err) {console.log(err)})
+    .finally(function() {
+      mongoose.connection.close();
     });
